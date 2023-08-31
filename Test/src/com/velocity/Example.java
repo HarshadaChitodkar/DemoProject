38f0de9413e85548dd39d3cf3e2e27203ff61780
Harshada <chitoharshada@gmail.com>
2023-08-31 21:01:51 +0530

    arraylist

--- Test/src/com/velocity/Example.java
+++ Test/src/com/velocity/Example.java
@@ -1,5 +1,13 @@
 package com.velocity;
 
-public class Example {
+import java.util.ArrayList;
 
+public class Example {
+    public static void main(String args[]) {
+    	ArrayList al= new ArrayList();
+    	al.add(20);
+    	al.add(50);
+    	al.add(70);
+    	System.out.println(al);
+    }
 }
